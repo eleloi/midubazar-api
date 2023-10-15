e3722b600b6b17578958b35f9c532a77673eda2c
Eloi Vallverdú <eleloi@gmail.com>
2023-10-15 23:30:15 +0200

    Added express-requests-logger for auditing API requests

--- src/product/infrastructure/api/index.ts
+++ src/product/infrastructure/api/index.ts
@@ -1,10 +1,12 @@
 import express, { json } from 'express'
+import audit from 'express-requests-logger'
 
 import { createRouter } from './routes.ts'
 
 export function createApp() {
   const app = express()
   app.use(json())
+  app.use(audit())
   app.disable('x-powered-by')
 
   const router = createRouter()
